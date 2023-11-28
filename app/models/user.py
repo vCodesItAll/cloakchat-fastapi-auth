b8a6b8c371af3dbf0dcd94f4a85a587dc53b4c23
Vanessa Dickey <vcdickey@gmail.com>
2023-11-28 15:09:56 +0000

    first start with UserModel and MessageModel classes, moving on to schemas now

--- app/models/user.py
+++ app/models/user.py
@@ -7,12 +7,15 @@ from app.db.base_class import Base
 class UserModel(Base):
     __tablename__ = "users"
 
+    # setting index to true makes id, username, and email easier to search amongst large data
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
+    # not allowing null so users must have a username
+    username = Column(String, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
+    # don't think I need the is_superuser code below
+    # is_superuser = Column(Boolean(), default=False)
     relationship()
 
     def to_schema(self):
@@ -22,5 +25,13 @@ class UserModel(Base):
             email=self.email,
             hashed_password=self.hashed_password,
             is_active=self.is_active,
-            is_superuser=self.is_superuser
+            # don't think I need the is_superuser code below
+            # is_superuser=self.is_superuser
         )
+class MessageModel(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    receiver_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    message_content = Column(String, nullable=False)
+    sent_at = Column(DateTime, nullable=False)
