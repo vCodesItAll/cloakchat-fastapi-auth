31dc881f98e249a3a44da836434c7110c38b3515
Vanessa Dickey <vcdickey@gmail.com>
2023-12-07 18:59:42 +0000

    cleaned up code

--- app/controllers/FriendController.py
+++ app/controllers/FriendController.py
@@ -1,23 +1,21 @@
 from typing import Any, Dict, Optional, Union
-
 from sqlalchemy.orm import Session
-
 from app.core.security import get_password_hash, verify_password
 from app.controllers.BaseController import BaseController
-from app.models.user import User
-from app.schemas.user import UserCreate, UserUpdate
+from app.models.Friend import Friend
+from app.schemas.Friend import FriendCreate, FriendUpdate
 
 
-class UserController(BaseController[User, UserCreate, UserUpdate]):
-    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-        return db.query(User).filter(User.email == email).first()
+class FriendController(BaseController[Friend, FriendCreate, FriendUpdate]):
+    def get_by_email(self, db: Session, *, email: str) -> Optional[Friend]:
+        return db.query(Friend).filter(Friend.email == email).first()
 
-    def create(self, db: Session, *, obj_in: UserCreate) -> User:
-        db_obj = User(
+    def create(self, db: Session, *, obj_in: FriendCreate) -> Friend:
+        db_obj = Friend(
             email=obj_in.email,
             hashed_password=get_password_hash(obj_in.password),
-            username=obj_in.username,
-            is_superuser=obj_in.is_superuser,
+            Friendname=obj_in.Friendname,
+            is_superFriend=obj_in.is_superFriend,
         )
         db.add(db_obj)
         db.commit()
@@ -25,8 +23,8 @@ class UserController(BaseController[User, UserCreate, UserUpdate]):
         return db_obj
 
     def update(
-        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
-    ) -> User:
+        self, db: Session, *, db_obj: Friend, obj_in: Union[FriendUpdate, Dict[str, Any]]
+    ) -> Friend:
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
@@ -37,19 +35,19 @@ class UserController(BaseController[User, UserCreate, UserUpdate]):
             update_data["hashed_password"] = hashed_password
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
-    def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
-        user = self.get_by_email(db, email=email)
-        if not user:
+    def authenticate(self, db: Session, *, email: str, password: str) -> Optional[Friend]:
+        friend = self.get_by_email(db, email=email)
+        if not friend:
             return None
-        if not verify_password(password, user.hashed_password):
+        if not verify_password(password, friend.hashed_password):
             return None
-        return user
+        return friend
 
-    def is_active(self, user: User) -> bool:
-        return user.is_active
+    def is_active(self, friend: Friend) -> bool:
+        return friend.is_active
 
-    def is_superuser(self, user: User) -> bool:
-        return user.is_superuser
+    def is_superFriend(self, friend: Friend) -> bool:
+        return friend.is_superFriend
 
 
-user = UserController(User)
+friend_controller = FriendController(Friend)
