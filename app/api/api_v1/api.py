388f0621b07865245ab63af3208dbb8121b6ea1d
Vanessa Dickey <vcdickey@gmail.com>
2023-12-07 18:54:28 +0000

    making FriendController.py

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import login, users, utils, add_friend
+from app.api.api_v1.endpoints import friend, login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # app.include_router(friends.router, prefix="/api/friends", tags=["friends"])
-api_router.include_router(add_friend.router, prefix="/friends", tags=["friends"])
+api_router.include_router(friend.router, prefix="/friends", tags=["friends"])
--- app/api/api_v1/endpoints/friend.py
+++ app/api/api_v1/endpoints/friend.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app import db
 from app.models import Friends, User
-from app.database import get_db
+
+
+
+router = APIRouter()
+
+def get_friend_by_id(friend_id: int):
+    # Implement the logic to retrieve a friend by their ID
+    # You can replace the following line with your actual implementation
+    return None
+
 
 router = APIRouter()
 
